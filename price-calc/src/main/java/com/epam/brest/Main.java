4b2f4c82609fc71065f2125a0e7eed0b6eb4e0c9
Aliaksandr_Fedasiuk <Aliaksandr_Fedasiuk@epam.com>
2021-02-08 22:55:53 +0300

    Fix issues.

--- price-calc/src/main/java/com/epam/brest/Main.java
+++ price-calc/src/main/java/com/epam/brest/Main.java
@@ -6,7 +6,6 @@ import com.epam.brest.selector.PriceSelector;
 
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -28,7 +27,7 @@ public class Main {
 
         PriceSelector priceSelector = new PriceSelector();
 
-        Double[] enteredValues = new Double[2];
+        BigDecimal[] enteredValues = new BigDecimal[2];
         Scanner scanner = new Scanner(System.in);
         String inputValue;
 
@@ -43,26 +42,30 @@ public class Main {
             } else if (i == 1) {
                 System.out.println("Please, enter weight:");
             }
+
             inputValue = scanner.next();
             if (inputValue.equalsIgnoreCase("Q")) {
                 break;
             } else if (isCorrectDoubleValue(inputValue)) {
-                enteredValues[i] = Double.parseDouble(inputValue);
+                enteredValues[i] = new BigDecimal(inputValue);
                 i++;
             } else {
                 System.out.println("Incorrect value: " + inputValue);
             }
 
-            BigDecimal price = priceSelector.selectPriceValue(distancePriceMap, new BigInteger(enteredValues[0].toString()));
-            System.out.println("Price: " + price);
-
             if (i == 2) {
-                Double result = enteredValues[0] * price.doubleValue()
-                        + enteredValues[2] * enteredValues[3];
-                System.out.println("Result : " + result);
-            }
+                BigDecimal distancePrice = priceSelector.selectPriceValue(distancePriceMap, enteredValues[0].toBigInteger());
+                System.out.println("Distance price: " + distancePrice);
 
-        } while (i < 2);
+                BigDecimal weightPrice = priceSelector.selectPriceValue(weightPriceMap, enteredValues[1].toBigInteger());
+                System.out.println("Weight price: " + weightPrice);
+
+                BigDecimal result = enteredValues[0].multiply(distancePrice).add(enteredValues[1].multiply(weightPrice));
+                System.out.println("Result : " + result);
+
+                i = 0;
+            }
+        } while (true);
     }
 
     private static boolean isCorrectDoubleValue(String value) {
